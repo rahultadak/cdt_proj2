727e8ff236a74e215aa8f682dfb9c851e093a422
Rahul Tadakamadla <rahultadak91@gmail.com>
2014-11-06 16:53:58 -0500

    introducing predictor function
    1. Added transaction class
    2. Added 'predict' function in the predictor class
    3. Updated infra for hybrid predictor

--- classes.h
+++ classes.h
@@ -1,13 +1,15 @@
 #include <vector>
+#include <cmath>
 
 using namespace std;
+int Debug = 1;
 
 class PCounter{
     public:
 
         PCounter()
         {
-            state = 1;
+            state = 2;
         }
         
         int present_state()
@@ -38,19 +40,50 @@ class PCounter{
 
 class predictor{
     private:
-        int index;
+        int index_bits, index_offset;
         int ghr_size;
         vector<PCounter> counters;
 
     public:
         predictor(int in_size, int in_ghr)
         {
-            index = in_size;
+            index_bits = in_size;
+            index_offset = pow(2,index_bits) - 1;
             ghr_size = in_ghr;
 
-            counters.resize(2^index);
+            counters.resize(pow(2,index_bits));
         }
-            
+
+        int num_counters()
+        {   return counters.size();}
+        
+        void predict(int addr)
+        {
+            addr = addr / 4; //Removing the last 2 bits
+            int index = addr & index_offset;
+            //For gshare, we need to add the GHR component
+            //Update the counters
+            //Update the GHR
+            if (Debug) cout << dec << index;
+        }
 
 };
+
+class Transaction{
+    private:
+        int type,addr;
+        
+    public:
+        void setType(int x) { type = x;}
+        int tranType() { return type;}
+
+        void setAddr(const string &x) 
+        {
+            istringstream adr_buf(x.substr(0,6));
+            adr_buf>>hex>>addr;
+        }
+        int retAddr() { return addr; }
+
+};
+
 
--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include <string>
 #include <cstdlib>
 
@@ -9,9 +10,12 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int index_size, ghr_size;
+    int type;
+    ifstream trace;
     std::string input (argv[1]), bimodal("bimodal"), gshare("gshare"), hybrid("hybrid");
     
+    predictor *p_bm, *p_gs ;
+
     if (!input.compare(bimodal))
     {
         if (argc != 6)
@@ -21,7 +25,9 @@ int main(int argc, char *argv[])
             exit(EXIT_FAILURE);
         }
 
-        predictor* p = new predictor(atoi(argv[2]),0);
+        p_bm = new predictor(atoi(argv[2]),0);
+        type = 1;
+        trace.open(argv[5]); 
     }
     else if (!input.compare(gshare))
     {   
@@ -32,7 +38,10 @@ int main(int argc, char *argv[])
             exit(EXIT_FAILURE);
         }
 
-        predictor* p = new predictor(atoi(argv[2]),atoi(argv[3]));
+        p_gs = new predictor(atoi(argv[2]),atoi(argv[3]));
+        type = 2;
+        trace.open(argv[6]); 
+
     }
     else if (!input.compare(hybrid))
     {   
@@ -44,8 +53,52 @@ int main(int argc, char *argv[])
         }
 
         //Another class for hybrid?
-        predictor* p = new predictor(atoi(argv[2]),atoi(argv[3]));
+        p_bm = new predictor(atoi(argv[2]),atoi(argv[3]));
+        type = 3;
+        trace.open(argv[8]); 
+
+    } 
+
+    if (!trace)
+    {
+        cerr << "File not found, please check" << endl;
+        exit(1);
     }
+
+    int tran_cnt = 0;
+    Transaction InTran;
+    while(trace)
+    {
+        tran_cnt++;
+        if (Debug) cout << dec<<  tran_cnt << ". PC: ";
+        //taking input
+        string strIn;
+        //getting the line
+        getline(trace, strIn);
+        //breaking from the while loop if this is the last line of the file.
+        if (trace.eof()) break;
+
+        InTran.setAddr(strIn);
+        //Setting the type of transaction
+        if (strIn[7] == 't')
+        {
+            InTran.setType(1);
+            if (Debug) cout << hex << InTran.retAddr() << " t" << endl;
+        }
+        else if (strIn[7] == 'n')
+        {
+            InTran.setType(0);
+            if (Debug) cout << hex << InTran.retAddr() << " n"  << endl;
+        }
+
+        if (type == 1) 
+        {
+            if (Debug) cout << "BIMODAL index: ";
+            p_bm->predict(InTran.retAddr());
+            if (Debug) cout << endl;
+        }
+    }
+
     return 0;
 }
 
